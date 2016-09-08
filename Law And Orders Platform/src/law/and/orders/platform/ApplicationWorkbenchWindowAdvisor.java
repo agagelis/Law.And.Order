c55993bbb78f6dbd3244e1ee21d1191ae3f05990
agagelis <agagelis@gmail.com>
2016-09-09 01:22:26 +0300

    gui additions

--- Law And Orders Platform/src/law/and/orders/platform/ApplicationWorkbenchWindowAdvisor.java
+++ Law And Orders Platform/src/law/and/orders/platform/ApplicationWorkbenchWindowAdvisor.java
@@ -19,7 +19,7 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 
 	public void preWindowOpen() {
 		IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
-		configurer.setInitialSize(new Point(400, 300));
+		configurer.setInitialSize(new Point(1400, 1300));
 		configurer.setShowCoolBar(true);
 		configurer.setShowStatusLine(false);
 		configurer.setTitle("Law And Orders");
