49e42a644190662dd9e198387987b512b0cf6505
agagelis <agagelis@gmail.com>
2016-09-30 01:15:48 +0300

    ok

--- Common UI Bundle/src/common/ui/bundle/ui/constants/UIConstantsViewIDs.java
+++ Common UI Bundle/src/common/ui/bundle/ui/constants/UIConstantsViewIDs.java
@@ -1,5 +1,9 @@
 package common.ui.bundle.ui.constants;
 
 public class UIConstantsViewIDs {
+	
+	public static final String CustomerManagementCustomersView = "customer.management.bundle.views.CustomersView";
+	public static final String CustomerManagementCustomerInformationView = "customer.management.bundle.views.CustomerInformationView";
+	
 
 }
