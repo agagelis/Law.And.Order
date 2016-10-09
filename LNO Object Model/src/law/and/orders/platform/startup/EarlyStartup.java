cf1781d75922c88969b022ebdcb282aeb92b6db6
agagelis <agagelis@gmail.com>
2016-10-09 10:21:03 +0300

    added DB initialization to be executed on platform start

---
+++ LNO Object Model/src/law/and/orders/platform/startup/EarlyStartup.java
@@ -0,0 +1,14 @@
+package law.and.orders.platform.startup;
+
+import law.and.orders.platform.dbaccess.JPAUtil;
+
+import org.eclipse.ui.IStartup;
+
+public class EarlyStartup implements IStartup {
+
+	@Override
+	public void earlyStartup() {
+		JPAUtil.getSessionFactory();
+	}
+
+}
